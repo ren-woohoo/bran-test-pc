2ea3bd26caf691b1f6cb34e2dfc39f0356c6a117
RCL1111 <rcl_love@163.com>
2017-06-09 18:11:59 +0800

    1：测试git；

--- main.cpp
+++ main.cpp
@@ -18,9 +18,6 @@ int main(int argc, char *argv[])
    #endif
     QApplication a(argc, argv);
     WidgetMain w;
-    QString str ="\xE9\x9D\x9E\xE6\xB3\x95\xE8\xAF\xB7\xE6\xB1\x82\xEF\xBC\x8C\xE5\x8A\xA0\xE5\xAF\x86\xE4\xBF\xA1\xE6\x81\xAF\xE6\x9C\x89\xE9\x94\x99";
-    qDebug()<<str;
-//    WidgetDebugInfo w;
     w.show();
     return a.exec();
 }
